7c7819555410b1d82b5ddae8681916a3089d4786
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-05-15 16:51:35 +0100

    Setup core game script (hacker.cs) & implemented main menu function.

---
+++ Assets/Scripts/Hacker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+
+    }
+
+    private static void ShowMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Welcome to HaxxOSv2.0.1");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Scanning for vulnerable devices...");
+        Terminal.WriteLine("Devices Located!");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Listing Devices:");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("1. [EASY]   Mrs_Bermont_Home_Laptop");
+        Terminal.WriteLine("2. [MEDIUM] Dads-iPhone-10s-jailbroken");
+        Terminal.WriteLine("3. [HARD]   Local_Bank_WiFi2");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Please provide the target number to initiate attack");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
