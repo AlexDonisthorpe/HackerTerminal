1a9c3a4e045a73693dc25f5573dc8384083398d9
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-05-15 20:02:20 +0100

    Added a state tracker variable to keep track of the current player selected level

--- Assets/Scripts/Hacker.cs
+++ Assets/Scripts/Hacker.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    string greeting = "Hello anon";
+    int level;
     // Start is called before the first frame update
     void Start()
     {
-        var greeting = "Hello anon";
         ShowMainMenu(greeting);
     }
 
@@ -24,14 +26,43 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("");
         Terminal.WriteLine("1. [EASY]   Mrs_Bermont_Home_Laptop");
         Terminal.WriteLine("2. [MEDIUM] Dads-iPhone-10s-jailbroken");
-        Terminal.WriteLine("3. [HARD]   Local_Bank_WiFi2");
+        Terminal.WriteLine("3. [HARD]   Local_Bank_WiFi2-Hub");
         Terminal.WriteLine("");
         Terminal.WriteLine("Please provide the target number to initiate attack");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnUserInput(string input)
     {
-        
+        if(input == "menu")
+        {
+            ShowMainMenu(greeting);
+        } else if(input == "1")
+        {
+            level = 1;
+            StartGame();
+        } else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        } else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        } else if (input == "007")
+        {
+            ShowMainMenu("Greetings, Mr Bond");
+        } else if (input == "1337")
+        {
+            Terminal.WriteLine("Administrator Mode Activated");
+        }
+        else
+        {
+            Terminal.WriteLine("Please enter a valid option (1, 2 or 3)");
+        }
+    }
+
+    void StartGame()
+    {
+        Terminal.WriteLine("The current selected level is " + level);
     }
 }
